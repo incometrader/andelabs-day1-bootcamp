864d14f4e5d5f7c965232e31f9cb0ea193889a7a
incometrader <igdefinition@gmail.com>
2017-02-13 12:48:04 +0100

    [dataTypes] - add functions to pass tests

--- src/datatypes.js
+++ src/datatypes.js
@@ -1,7 +1,5 @@
 function dataTypes(argument){
-	if (Array.isArray(argument)){
-		return argument[2];
-	}
+
 	if (typeof argument === "object" || typeof argument === "undefined"){
 		return "no value";
 	}
@@ -10,18 +8,6 @@ function dataTypes(argument){
 	}
 	if (typeof argument === "boolean"){
 		return argument;
-	}
-	if (typeof argument === "number"){
-		if (argument > 100){
-			return "more than 100";
-		} else if (argument < 100){
-			return "less than 100";
-		} else if (argument === 100){
-			return "equal to 100";
-		}
-	}
-	if (typeof argument === "function"){
-		return argument(true);
 	}
 }
 
